cd518cb6c5c12f35e5fc17e5199dfbe3b52c10a9
James Beeber <jhbeeber@gmail.com>
2024-01-27 20:13:10 +0000

    Added main controller and defined basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root "main#index"
+  
+  get 'main/index'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/delete'
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
